ade1e5e06485626df78b19b2abd45cb799312d97
hiwi-dci-student <hiwot.abdi@dci.education>
2023-06-19 21:40:39 +0200

    working on home css

--- client/src/component/Home/Home.js
+++ client/src/component/Home/Home.js
@@ -142,14 +142,7 @@ const HomePage = () => {
         {/* Display testimonials or reviews */}
       </section>
 
-      {/* <footer className="footer">
-        <p>&copy; 2023 ዘፈን Zefen All rights reserved.</p>
-        <nav className="footer-nav">
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/privacy">Privacy Policy</Link>
-        </nav>
-      </footer> */}
+    
     </div>
   );
 };
