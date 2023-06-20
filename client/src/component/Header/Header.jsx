fba02faf79e3c41b547cf5fe2f22d1f30051f43d
hiwi-dci-student <hiwot.abdi@dci.education>
2023-06-21 00:22:54 +0200

    fix: fixed the navbar responsiveness

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,88 +1,113 @@
-
-
 //-------------------------------------------------------------------------------------------------
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import UserContext from "../userContext/UserContext";
 import axios from "../../util/axiosInstance";
-import './Header.css';
+import "./Header.css";
 
 export default function Header() {
   const { user, setUser } = useContext(UserContext);
 
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const handleSignOut = async () => {
     try {
       // make a request to your server to end the user's session
-      await axios.get('/api/users/signout');
+      await axios.get("/api/users/signout");
 
       // clear the user's data from the context
       setUser(null);
     } catch (error) {
-      console.error('There was an error signing out', error);
+      console.error("There was an error signing out", error);
     }
   };
 
+  const handleNavToggle = () => {
+    console.log("is Open");
+    setIsNavOpen(!isNavOpen);
+    console.log("isOpen", isNavOpen);
+  };
+
   return (
-    <div className="header">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          ዘፈን Zefen
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light-blue">
+      <Link className="navbar-brand" to="/">
+        ዘፈን Zefen
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={handleNavToggle}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div  className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`} id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/categories">
+              Music Genres
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/searchArtist">
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/instruments">
+              Instruments
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/addSong">
+              Add Song
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          {user ? (
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <div className="d-flex align-items-center">
+                <img
+                  src={user.avatar}
+                  alt="User avatar"
+                  className="user-avatar"
+                />
+                <Link className="nav-link" to="/" onClick={handleSignOut}>
+                  Sign Out
+                </Link>
+              </div>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/categories">Music Genres</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/searchArtist">Search</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/instruments">Instruments</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/addSong">Add Song</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            {user ? (
+          ) : (
+            <>
               <li className="nav-item">
-                <div className="d-flex align-items-center">
-                  <img src={user.avatar} alt="User avatar" className="user-avatar" />
-                  <Link className="nav-link" to="/" onClick={handleSignOut}>Sign Out</Link>
-                </div>
+                <Link className="nav-link" to="/signin">
+                  Login
+                </Link>
               </li>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signin">Login</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signup">Signup</Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </nav>
-    </div>
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">
+                  Signup
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
   );
 }
 
 //-------------------------------------------------------------------------------
-
-
