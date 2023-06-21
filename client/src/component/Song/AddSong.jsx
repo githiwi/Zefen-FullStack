ec340e4ece4c071d2fe6342fd70dd381f9ae1557
hiwi-dci-student <hiwot.abdi@dci.education>
2023-06-21 11:41:49 +0200

    add song page

--- client/src/component/Song/AddSong.jsx
+++ client/src/component/Song/AddSong.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../../util/axiosInstance.js';
-import './AddSong.css'
+import React, { useState, useEffect } from "react";
+import axios from "../../util/axiosInstance.js";
+import "./AddSong.css";
+
 const AddSong = () => {
   const [title, setTitle] = useState("");
   const [artist, setArtist] = useState("");
@@ -8,54 +9,98 @@ const AddSong = () => {
   const [gener, setGener] = useState("");
 
   const [artists, setArtists] = useState([]);
-  const [genres, setGenres] = useState(["Bati", "Ambasel", "Anchihoye", "Tizita"]);
+  const [genres, setGenres] = useState([
+    "Bati",
+    "Ambasel",
+    "Anchihoye",
+    "Tizita",
+  ]);
 
   useEffect(() => {
     // Load artists from your API when the component mounts
-    axios.get('/api/artist/allArtist')
-      .then(res => setArtists(res.data))
-      .catch(err => console.error(err));
+    axios
+      .get("/api/artist/allArtist")
+      .then((res) => setArtists(res.data))
+      .catch((err) => console.error(err));
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const songData = new FormData(e.target);
-    songData.append('title', title);
-    songData.append('artists', artist);
+    songData.append("title", title);
+    songData.append("artists", artist);
     //songData.append('musicUrl', musicUrl);
-    songData.append('gener', gener);
-    console.log("why",songData)
-    axios.post('/api/song/addSong', songData).then(res => {
-      console.log("Song created successfully", res.data);
-    }).catch(err => {
-      console.error(err);
-    });
-  }
+    songData.append("gener", gener);
+    console.log("why", songData);
+    axios
+      .post("/api/song/addSong", songData)
+      .then((res) => {
+        console.log("Song created successfully", res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
-    <form onSubmit={handleSubmit} className='form-container'>
-      <label>
-        Title:
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </label>
-      <label>
-        Artist:
-        <select value={artist} onChange={(e) => setArtist(e.target.value)}>
-          {artists.map(a => <option key={a._id} value={a._id}>{`${a.firstName} ${a.lastName}`}</option>)}
-        </select>
-      </label>
-      <label>
-        Music URL:
-        <input type="file" name="audio" onChange={(e) => setMusicUrl(e.target.files[0])} />
-      </label>
-      <label>
-        Genre:
-        <select value={gener} onChange={(e) => setGener(e.target.value)}>
-          {genres.map(g => <option key={g} value={g}>{g}</option>)}
-        </select>
-      </label>
-      <input type="submit" value="Add Song" />
+    <form onSubmit={handleSubmit} className="add-song-form">
+      <div className="form-row">
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="artist">Artist</label>
+          <select
+            id="artist"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+          >
+            {artists.map((a) => (
+              <option
+                key={a._id}
+                value={a._id}
+              >{`${a.firstName} ${a.lastName}`}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group">
+          <label htmlFor="musicUrl">Music URL</label>
+          <input
+            type="file"
+            id="musicUrl"
+            name="audio"
+            onChange={(e) => setMusicUrl(e.target.files[0])}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="genre">Genre</label>
+          <select
+            id="genre"
+            value={gener}
+            onChange={(e) => setGener(e.target.value)}
+          >
+            {genres.map((g) => (
+              <option key={g} value={g}>
+                {g}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group">
+          <input type="submit" value="Add Song" className="add-song-submit" />
+        </div>
+      </div>
     </form>
   );
 };
